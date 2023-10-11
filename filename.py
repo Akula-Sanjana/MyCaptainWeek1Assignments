58e8050575a811b30179a3058318585a4a97f59c
Akula-Sanjana <146711990+Akula-Sanjana@users.noreply.github.com>
2023-10-11 11:49:35 +0530

    Add files via upload

---
+++ filename.py
@@ -0,0 +1,3 @@
+filename = input("Input the Filename: ")
+base_name, extension = filename.rsplit('.', 1)#splits base name and extension 
+print(f"The extension of the file is: '{extension}'")#prints the extension name
